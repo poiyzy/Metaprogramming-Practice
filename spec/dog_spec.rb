deb50b22c281b15aa440995a492224defcd48e27
Roy Young <zhuoyuyang@gmail.com>
2013-04-17 09:56:14 +0800

    Cover tests of step1

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe Dog do
-  context "having method" do
-    it "shows MSGS attributes" do
-      dog = Dog.new("Qiqi")
+  let(:dog) { Dog.new("Qiqi") }
+
+  context "having abilities" do
+    it "shows is doing something" do
       dog.can :dance
       dog.dance.should == "Qiqi is dancing"
+    end
+  end
+
+  context "Not having abilities" do
+    context "call abilities included in MSGS" do
+      it "shows does not understand" do
+        dog.dance.should == "Qiqi doesn't understand dance"
+      end
+    end
+
+    context "call abilities excluded in MSGS" do
+      it "shows does not understand" do
+        dog.some_action.should == "Qiqi doesn't understand some_action"
+      end
     end
   end
 end
